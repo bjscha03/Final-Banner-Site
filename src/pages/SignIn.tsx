4ea1e82a9930ccdf6b161f7eb0411d6e1d0ba9d8
Brandon Schaefer <brandonschaefer@Mac.lan>
2025-10-07 14:31:39 -0400

    Update sign-in banner to final image
    - Updated banner image to Screenshot_2025-10-07_at_2.29.47_PM_wegqxg.png
    - Full image visible on all screen sizes with object-contain

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -203,7 +203,7 @@ const SignIn: React.FC = () => {
       {/* Bottom Banner Image */}
       <div className="w-full mt-auto">
         <img 
-          src="https://res.cloudinary.com/dtrxl120u/image/upload/v1759861550/Screenshot_2025-10-07_at_2.25.43_PM_vhkshg.png"
+          src="https://res.cloudinary.com/dtrxl120u/image/upload/v1759861799/Screenshot_2025-10-07_at_2.29.47_PM_wegqxg.png"
           alt="Banner printing services"
           className="w-full h-auto object-contain"
         />
