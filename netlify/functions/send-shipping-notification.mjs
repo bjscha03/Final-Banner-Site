3362285e9169c355369bdb5fc5c374d793ef1fb1
bjscha03 <141862943+bjscha03@users.noreply.github.com>
2026-07-28 19:27:40 -0400

    Bundle legacy shipping email runtime dependencies

--- netlify/functions/send-shipping-notification.mjs
+++ netlify/functions/send-shipping-notification.mjs
@@ -1,5 +1,6 @@
+import '@neondatabase/serverless';
+import 'resend';
 import { withLambda } from '@netlify/aws-lambda-compat';
 import legacyModule from './_shared/legacy/send-shipping-notification.cjs';
 
 export default withLambda(legacyModule.handler);
-
