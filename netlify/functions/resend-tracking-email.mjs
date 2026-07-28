9d0674712f20198ee23684cccecceaa1b943ae57
bjscha03 <141862943+bjscha03@users.noreply.github.com>
2026-07-28 19:28:15 -0400

    Use canonical tracking email resend path

--- netlify/functions/resend-tracking-email.mjs
+++ netlify/functions/resend-tracking-email.mjs
@@ -1,5 +1,6 @@
+import '@neondatabase/serverless';
+import 'resend';
 import { withLambda } from '@netlify/aws-lambda-compat';
-import legacyModule from './_shared/legacy/resend-tracking-email.cjs';
+import trackingEmailModule from './_shared/tracking-email-handler.cjs';
 
-export default withLambda(legacyModule.handler);
-
+export default withLambda(trackingEmailModule.handler);
