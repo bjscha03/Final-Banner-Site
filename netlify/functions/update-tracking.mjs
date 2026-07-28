f494747980e58ad96827822584a53ae6dfe74956
bjscha03 <141862943+bjscha03@users.noreply.github.com>
2026-07-28 19:28:20 -0400

    Bundle tracking update database dependency

--- netlify/functions/update-tracking.mjs
+++ netlify/functions/update-tracking.mjs
@@ -1,5 +1,5 @@
+import '@neondatabase/serverless';
 import { withLambda } from '@netlify/aws-lambda-compat';
 import legacyModule from './_shared/legacy/update-tracking.cjs';
 
 export default withLambda(legacyModule.handler);
-
